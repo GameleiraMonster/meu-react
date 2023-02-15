b29ae35c50dd605268b82f77283b6569934f02bb
Jânio Gameleira <jsgj1@aluno.ifal.edu.br>
2023-02-15 20:01:28 -0300

    add componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './components/HelloWorld'
 function App() {
+
+  const name = 'Jânio'
+  const newName = name.toUpperCase()
+  function sum (a,b){
+    return(a+b) 
+  }
+
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className="App">
-      <h1>Olá react!</h1>
-      <p>Meu primeiro App</p>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(2,3)}</p>
+      <img src={url} alt="Minha moto"/>
+      <HelloWorld />
     </div>
-  );
+  )
 }
 
 export default App;
