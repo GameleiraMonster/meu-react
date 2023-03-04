88c06edfbcd65d1c46e422201ea01253d07055df
Jânio Gameleira <jsgj1@aluno.ifal.edu.br>
2023-03-04 15:51:41 -0300

    Todos os componentes básicos do react foram adicionados

---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import Item from "./Item"
+
+function List (){
+    return(
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <Item marca="Ferrari" ano_lancamento={1985}/>
+                <Item marca="lamborgini" ano_lancamento={1964}/>
+                <Item />
+            </ul>
+        </>
+    )
+}
+
+export default List
